4d23a211fb0f82bee04a0d414a63da35dc8e451d
ionescuig <ionescuig@yahoo.com>
2018-02-21 03:59:23 +0000

    Template View part 1

--- src/restaurants/views.py
+++ src/restaurants/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.views import View
+from django.views.generic import TemplateView
 
 
 def home(request):
@@ -21,3 +23,9 @@ def contact(request):
 
     }
     return render(request, "contact.html", context)
+
+
+class ContactView(View):
+    def get(self, request, *args, **kwargs):
+        context = {}
+        return render(request, "contact.html", context)
