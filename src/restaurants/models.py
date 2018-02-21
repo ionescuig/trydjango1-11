28c57b7ad6c2beda71abaf3a25cd68d7ce547ece
ionescuig <ionescuig@yahoo.com>
2018-02-21 21:15:17 +0000

    Displaying Saved Data

--- src/restaurants/models.py
+++ src/restaurants/models.py
@@ -2,5 +2,11 @@ from django.db import models
 
 
 class RestaurantLocation(models.Model):
-    name        = models.CharField(max_length=120)
-    location    = models.CharField(max_length=120, null=True, blank=True)
+    name = models.CharField(max_length=120)
+    location = models.CharField(max_length=120, null=True, blank=True)
+    category = models.CharField(max_length=120, null=True, blank=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
