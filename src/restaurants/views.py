f725427eda2c45675ccec0cdc5d4404c57bb4a11
ionescuig <ionescuig@yahoo.com>
2018-02-21 23:23:37 +0000

    Restaurant Profile Detail

--- src/restaurants/views.py
+++ src/restaurants/views.py
@@ -1,9 +1,9 @@
 from .models import RestaurantLocation
 from django.db.models import Q
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import View
-from django.views.generic import TemplateView, ListView
+from django.views.generic import TemplateView, ListView, DetailView
 
 
 def restaurant_listview(request):
@@ -26,3 +26,12 @@ class RestaurantListView(ListView):
         else:
             queryset = RestaurantLocation.objects.all()
         return queryset
+
+
+class RestaurantDetailView(DetailView):
+    queryset = RestaurantLocation.objects.all()
+
+    def get_object(self, *args, **kwargs):
+        rest_id = self.kwargs.get('rest_id')
+        obj = get_object_or_404(RestaurantLocation, id=rest_id) # pk = rest_id
+        return obj
