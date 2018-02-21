07d12a2c1517d5fc36194736d8fc324f30adfade
ionescuig <ionescuig@yahoo.com>
2018-02-21 16:35:07 +0000

    Remembering things with Models

--- src/restaurants/views.py
+++ src/restaurants/views.py
@@ -1,31 +1,26 @@
+import random
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.views import View
 from django.views.generic import TemplateView
 
 
-def home(request):
-    context = {
+class HomeView(TemplateView):
+    template_name = 'home.html'
 
-    }
-    return render(request, "home.html", context)
-
-
-def about(request):
-    context = {
-
-    }
-    return render(request, "about.html", context)
-
-
-def contact(request):
-    context = {
-
-    }
-    return render(request, "contact.html", context)
-
-
-class ContactView(View):
-    def get(self, request, *args, **kwargs):
-        context = {}
-        return render(request, "contact.html", context)
+    def get_context_data(self, *args, **kwargs):
+        context = super(HomeView, self).get_context_data(*args, **kwargs)
+        num = None
+        some_list = [
+            random.randint(0, 100000000),
+            random.randint(0, 100000000),
+            random.randint(0, 100000000)
+        ]
+        condition_bool_item = True
+        if condition_bool_item:
+            num = random.randint(0, 100000000)
+        context = {
+            "num": num,
+            "some_list": some_list
+        }
+        return context
