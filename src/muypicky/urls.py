97f3b4513bdac975e828b9e59853eb21571d00bc
ionescuig <ionescuig@yahoo.com>
2018-03-01 01:04:04 +0000

    Added follow user option

--- src/muypicky/urls.py
+++ src/muypicky/urls.py
@@ -3,11 +3,14 @@ from django.contrib import admin
 from django.contrib.auth.views import LoginView
 from django.views.generic import TemplateView
 
+from profiles.views import ProfileFollowToggle
+
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', TemplateView.as_view(template_name='home.html'), name='home'),
     url(r'^login/$', LoginView.as_view(), name='login'),
+    url(r'^profile-follow/$', ProfileFollowToggle.as_view(), name='follow'),
     url(r'^restaurants/', include('restaurants.urls', namespace='restaurants')),
     url(r'^u/', include('profiles.urls', namespace='profile')),
     url(r'^items/', include('menus.urls', namespace='menus')),
--- src/profiles/views.py
+++ src/profiles/views.py
@@ -1,11 +1,20 @@
 from django.contrib.auth import get_user_model
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import Http404
-from django.shortcuts import render, get_object_or_404
-from django.views.generic import DetailView
+from django.shortcuts import render, get_object_or_404, redirect
+from django.views.generic import DetailView, View
 
+from .models import Profile
 from restaurants.models import RestaurantLocation
 
 User = get_user_model()
+
+
+class ProfileFollowToggle(LoginRequiredMixin, View):
+    def post(self, request, *args, **kwargs):
+        username_to_toggle = request.POST.get('username').strip()
+        profile_, is_following = Profile.objects.toggle_follow(request.user, username_to_toggle)
+        return redirect(f'/u/{profile_.user.username}/')
 
 
 class ProfileDetailView(DetailView):
@@ -20,6 +29,10 @@ class ProfileDetailView(DetailView):
     def get_context_data(self, *args, **kwargs):
         context = super(ProfileDetailView, self).get_context_data(*args, **kwargs)
         user = context['user']
+        is_following = False
+        if user.profile in self.request.user.is_following.all():
+            is_following = True
+        context['is_following'] = is_following
         query = self.request.GET.get('q')
         qs = RestaurantLocation.objects.filter(owner=user).search(query)
         if qs.exists():
