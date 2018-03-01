8779a77e2ed4dcbaee3b42b6a9a329703921d694
ionescuig <ionescuig@yahoo.com>
2018-03-01 14:43:03 +0000

    Following Home Page Feed

--- src/muypicky/urls.py
+++ src/muypicky/urls.py
@@ -3,16 +3,17 @@ from django.contrib import admin
 from django.contrib.auth.views import LoginView
 from django.views.generic import TemplateView
 
+from menus.views import HomeView
 from profiles.views import ProfileFollowToggle
 
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', TemplateView.as_view(template_name='home.html'), name='home'),
+    url(r'^$', HomeView.as_view(), name='home'),
     url(r'^login/$', LoginView.as_view(), name='login'),
     url(r'^profile-follow/$', ProfileFollowToggle.as_view(), name='follow'),
     url(r'^restaurants/', include('restaurants.urls', namespace='restaurants')),
-    url(r'^u/', include('profiles.urls', namespace='profile')),
+    url(r'^u/', include('profiles.urls', namespace='profiles')),
     url(r'^items/', include('menus.urls', namespace='menus')),
     url(r'^about/$', TemplateView.as_view(template_name='about.html'), name='about'),
     url(r'^contact/$', TemplateView.as_view(template_name='contact.html'), name='contact'),
