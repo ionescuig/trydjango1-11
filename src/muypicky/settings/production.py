d275dde98962479e7abd9550fe5ca21fd41ec358
ionescuig <ionescuig@yahoo.com>
2018-03-23 06:48:07 +0000

    Corected the way admin page renders on heroku

--- src/muypicky/settings/production.py
+++ src/muypicky/settings/production.py
@@ -132,6 +132,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
+STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 
 LOGOUT_REDIRECT_URL = '/'
